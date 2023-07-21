a960ecb60c09448a1300c5b07bcab0cc876f26f2
Srikanth Phalgun Pyaraka <36931190+PyarakaSrikanth@users.noreply.github.com>
2023-07-22 02:17:11 +0530

    Update transformation.py

--- transformation.py
+++ transformation.py
@@ -24,7 +24,7 @@ def get_train_aug_transform(mu, sigma):
                                       fill_value=(mu),
                                       always_apply=True,
                                       p=0.50),   
-                             A.ToGray()   
+                             A.ToGray(),   
                              ToTensorV2(),
 ])
 
